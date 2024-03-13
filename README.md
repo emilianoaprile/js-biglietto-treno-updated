3d9c1c09b11074ca302b2aed72a8ef506649b183
EmilianoAprile <emilianoaprile77@icloud.com>
2024-03-13 17:28:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Calcolatore Biglietto Treno
+
+Questo progetto implementa un calcolatore di prezzo del biglietto del treno in base alla distanza, all'età del passeggero e applica sconti se necessario. Inoltre, consente di stampare i dettagli del passeggero sulla card del biglietto.
+
+## Funzionalità
+
+Il programma offre le seguenti funzionalità:
+
+- Prendere i valori degli input per nome, distanza e fascia d'età del passeggero.
+- Calcolare il prezzo del biglietto in base all'età e alla distanza.
+- Stampare i dettagli del passeggero sulla card del biglietto, inclusi sconti applicati, numero di carrozza e codice CP.
+- Pulire tutti i dati con il pulsante "Annulla".
+- Inserendo il nome completo nell'apposito input, ogni prima lettera del nome se viene scritta in minuscolo varrà stampata con le iniziali maiuscole.
+- Sono presenti i seguenti controlli:
+    - Il nome non può avere meno di 3 caratteri
+    - La distanza non può essere inferiore a 10 (km)
+
+## Utilizzo
+
+1. Inserisci il nome del passeggero, la distanza del viaggio e l'età nei rispettivi campi di input.
+2. Clicca su "Crea" per calcolare il prezzo del biglietto e stampare i dettagli del passeggero.
+3. Per cancellare tutti i dati, clicca su "Annulla".
+
+## Tecnologie Utilizzate
+
+Il progetto è stato sviluppato utilizzando HTML, CSS e JavaScript.
---
+++ js/app.js
@@ -0,0 +1,149 @@
+console.log('ok')
+
+// input form
+const nameInputElement = document.getElementById('name')
+const distanceInputElement = document.getElementById('distance')
+const ageInputElement = document.getElementById('age')
+
+console.log(nameInputElement, distanceInputElement, ageInputElement)
+
+// elementi DOM 
+const passengerNameElement = document.getElementById('ticket-name')
+const offerTypeElement = document.getElementById('offer-type')
+const wagonElement = document.getElementById('wagon-number')
+const codeElement = document.getElementById('cp-code')
+const finalPriceElement = document.getElementById('final-price')
+
+console.log(passengerNameElement, offerTypeElement, wagonElement, codeElement, finalPriceElement)
+
+// button crea - annulla
+const btnCreaElement = document.getElementById('btn-crea')
+const btnAnnullaElement = document.getElementById('btn-annulla')
+
+console.log(btnCreaElement, btnAnnullaElement)
+
+// evento crea
+btnCreaElement.addEventListener('click', function () {
+    console.log('ho cliccato')
+
+    // variabili 
+    const tariffa = 0.21
+    const distanza = parseInt(distanceInputElement.value)
+    const eta = parseInt(ageInputElement.value)
+    let prezzoBase = distanza * tariffa
+    const sconto20 = (prezzoBase / 100) * 20
+    const sconto40 = (prezzoBase / 100) * 40
+    let prezzoFinale
+
+    // calcolo del biglietto
+    if (eta === 0) {
+
+        prezzoFinale = prezzoBase - sconto20
+        displayElements()
+        capitalizeFirstLetter(nameInputElement)
+
+    } else if (eta === 2) {
+
+        prezzoFinale = prezzoBase - sconto40
+        displayElements()
+        capitalizeFirstLetter(nameInputElement)
+
+
+    } else if (eta === 1) {
+
+        prezzoFinale = prezzoBase
+        displayElements()
+        capitalizeFirstLetter(nameInputElement)
+    }
+
+
+    // controllo valori input
+    if (nameInputElement.value === '' || nameInputElement.value.length < 3) {
+
+        console.log('Il nome deve avere almeno 3 caratteri') // TODO controllo input
+
+        // const errorMessageElement = document.getElementById('errorName');
+        // errorMessageElement.innerHTML = 'Il nome deve avere almeno 3 lettere'
+
+        // setTimeout(function() {
+        //     errorMessageElement.style.display = 'none';
+        // }, 4000);
+
+        notDisplayElements()
+    }
+
+    if (distanceInputElement.value === '' || distanceInputElement.value < 10) {
+        console.log('La distanza minima è di 10km') // TODO controllo input
+        notDisplayElements()
+    }
+
+    // funzioni
+    function displayElements() {
+
+        if (eta === 0) {
+            offerTypeElement.innerHTML = 'Sconto 20%'
+        } else if (eta === 2) {
+            offerTypeElement.innerHTML = 'Sconto 40%'
+        } else if (eta === 1) {
+            offerTypeElement.innerHTML = 'Prezzo Pieno'
+        }
+
+        // passengerNameElement.innerHTML = nameInputElement.value
+        wagonElement.innerHTML = Math.floor(Math.random() * 9) + 1
+        codeElement.innerHTML = Math.floor(Math.random() * (10000 - 9000)) + 9000
+        finalPriceElement.innerHTML = prezzoFinale.toFixed(2) + '&euro;'
+
+    }
+
+    function notDisplayElements() {
+
+        passengerNameElement.innerHTML = ''
+        offerTypeElement.innerHTML = ''
+        wagonElement.innerHTML = ''
+        codeElement.innerHTML = ''
+        finalPriceElement.innerHTML = ''
+
+    }
+
+    function capitalizeFirstLetter(nameInputElement) {
+
+        let currentInputValue = nameInputElement.value;
+        const words = currentInputValue.split(' ');
+        console.log('array di parole:', words)
+
+        let capitalizedString = '';
+
+        for (let i = 0; i < words.length; i++) {
+            const word = words[i];
+            console.log('parola assegnata to indice:', word)
+
+            const capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1);
+            console.log('parola capitalizzata:', capitalizedWord)
+
+            capitalizedString += capitalizedWord + ' ';
+            console.log('concateno parole capit. con spazio', capitalizedString)
+        }
+
+        passengerNameElement.innerHTML = capitalizedString
+        return capitalizedString
+    }
+
+})
+
+// evento annulla
+btnAnnullaElement.addEventListener('click', function () {
+    // input
+    nameInputElement.value = ''
+    distanceInputElement.value = ''
+
+    // dati del biglietto
+    passengerNameElement.innerHTML = ''
+    offerTypeElement.innerHTML = ''
+    wagonElement.innerHTML = ''
+    codeElement.innerHTML = ''
+    finalPriceElement.innerHTML = ''
+
+})
+
+
+
